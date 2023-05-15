e5db8d1e68e0d8834ff02f9538cec0a6c717b72b
Nicholas Nash <nicholas.nash@cashtn.com>
2023-05-15 14:42:50 -0500

    completed project 1

---
+++ band-name-generator/main.py
@@ -0,0 +1,4 @@
+if __name__ == "__main__":
+    hometown_name = input('What\'s the name of the city that you grew up in?\n')
+    pet_name = input('What is your pet\'s name?\n')
+    print(f'Your band name could be {hometown_name} {pet_name}')
